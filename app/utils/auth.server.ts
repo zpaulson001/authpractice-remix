310196488ad049bedc7fbb362fd9518f22369e3f
Zac Paulson <zpaulson001@gmail.com>
2024-04-05 10:28:56 -0700

    Change from returning user object to returning user session

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -2,6 +2,7 @@ import { db } from 'db/db';
 import { users } from 'db/schema';
 import { eq } from 'drizzle-orm';
 import * as argon2 from 'argon2';
+import { createUserSession } from './session.server';
 
 export async function loginUser(username: string, password: string) {
   const user = await db.query.users.findFirst({
@@ -16,7 +17,7 @@ export async function loginUser(username: string, password: string) {
   const validPassword = await argon2.verify(user.passwordHash, password);
 
   if (validPassword) {
-    return { id: user.id, username };
+    return await createUserSession(user.id, '/success');
   } else {
     return null;
   }
@@ -34,12 +35,13 @@ export async function createUser(username: string, password: string) {
   }
 
   try {
-    const user = await db
+    const userReturn = await db
       .insert(users)
       .values({ username, passwordHash })
       .onConflictDoNothing()
-      .returning({ id: users.id, username: users.username });
-    return user;
+      .returning({ id: users.id });
+    const user = userReturn[0];
+    return await createUserSession(user.id, '/success');
   } catch (err) {
     console.error(err);
     return null;
