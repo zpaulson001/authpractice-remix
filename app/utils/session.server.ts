176ca595c206f781c18fe5580defee0a8d411ecb
Zac Paulson <zpaulson001@gmail.com>
2024-04-05 10:27:27 -0700

    Add utilities for creating session cookies

---
+++ app/utils/session.server.ts
@@ -0,0 +1,37 @@
+import { createCookieSessionStorage, redirect } from '@remix-run/node';
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET must be set');
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: '_session',
+    // normally you want this to be `secure: true`
+    // but that doesn't work on localhost for Safari
+    // https://web.dev/when-to-use-local-https/
+    secure: process.env.NODE_ENV === 'production',
+    secrets: [sessionSecret],
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: number, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set('userId', userId);
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  });
+}
+
+export async function getUserIdFromSession(request: Request) {
+  const session = await storage.getSession(request.headers.get('Cookie'));
+  const userId = session.get('userId');
+  return userId;
+}
