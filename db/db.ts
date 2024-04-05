50fe863c140bdb78bbf531d129dec205e7178b07
Zac Paulson <zpaulson001@gmail.com>
2024-04-05 10:30:14 -0700

    Add model for getting user by id

--- db/db.ts
+++ db/db.ts
@@ -1,6 +1,13 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import * as schema from './schema';
 import Database from 'better-sqlite3';
+import { eq } from 'drizzle-orm';
 
 const sqlite = new Database('./db/sqlite.db');
 export const db = drizzle(sqlite, { schema, logger: true });
+
+export async function getUserFromId(userId: number) {
+  return await db.query.users.findFirst({
+    where: eq(schema.users.id, userId),
+  });
+}
